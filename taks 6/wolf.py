f1ca84787689a0e6e38ff16d44888393572640d2
samiseveri <samiseveri.sjoberg@gmail.com>
2025-03-20 15:09:14 +0200

    Update wolf.py

--- taks 6/wolf.py
+++ taks 6/wolf.py
@@ -2,17 +2,17 @@ from mammal import Mammal
 
 class Wolf(Mammal):
     def __init__(self, pack_name: str):
-        super().__init__("Wolf")  # Passes species to Mammal
-        self.__pack_name = pack_name  # Encapsulated pack name
+        super().__init__("Wolf")  
+        self.__pack_name = pack_name  
 
     def make_sound(self):
-        print("*Awooooo!*")  # Overriding method
+        print("*Awooooo!*")  
 
     def another_make_sound(self):
         print("*Growl...*")
 
     def get_pack_name(self):
-        return self.__pack_name  # Getter for pack name
+        return self.__pack_name  
 
     def set_pack_name(self, pack_name):
-        self.__pack_name = pack_name  # Setter for pack name
+        self.__pack_name = pack_name  
